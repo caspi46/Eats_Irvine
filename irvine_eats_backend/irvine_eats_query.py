793f9ec39cae343912d96db182e35bbb96753d42
Joohyung Oh <124128396+Joohyung-accounting@users.noreply.github.com>
2025-08-21 22:26:07 -0700

    example of querying the data from irvine_eats.db

--- irvine_eats_backend/irvine_eats_query.py
+++ irvine_eats_backend/irvine_eats_query.py
@@ -1,1 +1,13 @@
+import sqlite3
 
+conn = sqlite3.connect('irvine_eats.db')
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM restaurants")
+
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.close()
