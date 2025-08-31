e4e7d1a2f2c881c2286c7931800bb9fa13eb7533
Joohyung Oh <124128396+Joohyung-accounting@users.noreply.github.com>
2025-08-30 23:19:03 -0700

    updated blueprints part

--- irvine_eats_backend/app.py
+++ irvine_eats_backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from routes import bp as users_bp
+from routes import USERS_BP, RESTAURANTS_BP, MENU_BP
 
 app = Flask(__name__)
-app.register_blueprint(users_bp)
+app.register_blueprint(USERS_BP)
+app.register_blueprint(RESTAURANTS_BP)
+app.register_blueprint(MENU_BP)
 
 if __name__ == "__main__":
     app.run(debug=True)
