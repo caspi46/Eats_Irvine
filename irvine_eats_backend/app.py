b68a5352fb66e0e1666f232187b3c3534de70c09
Kenny Kim <caspi4646@gmail.com>
2025-09-16 21:40:53 -0700

    added connection for flask and react

--- irvine_eats_backend/app.py
+++ irvine_eats_backend/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, send_from_directory 
 from routes import USERS_BP, RESTAURANTS_BP, MENU_BP
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder ="../irvine_eats_frontend/dist", static_url_path="/")
 app.register_blueprint(USERS_BP)
 app.register_blueprint(RESTAURANTS_BP)
 app.register_blueprint(MENU_BP)
 
+def index():
+    return send_from_directory(app.static_folder, "index.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) # add argument: port=5000 for connecting frontend 
